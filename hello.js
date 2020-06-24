d7bbba0a9121f951bd1f282b373948848c433bd1
thisisbird <thisisbirdhead@gmail.com>
2020-06-24 17:32:56 +0800

    2nd

---
+++ hello.js
@@ -0,0 +1,10 @@
+const helloWorld=()=>{
+    console.log("hello world!");
+}
+
+export const msg="hello world";
+
+export default {helloWorld};
+/*使用default必須要包成一個物件，export default可以和多個export同時存在。*/
+
+console.log(123);
