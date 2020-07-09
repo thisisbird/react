746ffe415d2b39c02c7f745494d2396df209943d
thisisbird <thisisbirdhead@gmail.com>
2020-07-09 17:53:47 +0800

    套bootstrap4

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -1,15 +1,34 @@
 import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
-import { Index, Books, Electronics } from './pages';
-
+// import { Index, Books, Electronics } from './pages';
+import { Navbar } from './components';
+import routes from './routes';
 class App extends Component {
   render() {
     return (
       <div className="container">
+        {/* Put Navbar Here */}
+        <Navbar />
+
         {/* The corresponding component will show here if the current URL matches the path */}
-        <Route path="/" exact component={Index} />
+        {/* <Route path="/" exact component={Index} />
         <Route path="/books" component={Books} />
-        <Route path="/electronics" component={Electronics} />
+        <Route path="/electronics" component={Electronics} /> */}
+
+         {/* Refactor for using routes config */}
+         {routes.map((route, i) => {
+            const { path, exact, routes } = route;
+            return (
+              <Route
+                key={i}
+                path={path}
+                exact={exact}
+                render={(routeProps) => (
+                  <route.component routes={routes} {...routeProps} />
+                )}
+              />
+            );
+          })}
       </div>
     );
   }
